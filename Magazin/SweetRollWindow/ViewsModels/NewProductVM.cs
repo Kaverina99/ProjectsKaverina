895341069236dfd23ba7a70b65d358854740062d
Kaverina99 <57800050+Kaverina99@users.noreply.github.com>
2021-09-16 21:38:14 +0300

    mm
    mm

---
+++ Magazin/SweetRollWindow/ViewsModels/NewProductVM.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BLL;
+
+namespace SweetRollWindow.ViewsModels
+{
+
+    public class NewProductVM : INotifyPropertyChanged
+    {
+        private ProduktModel newProduct;
+
+        public NewProductVM(ProduktModel p)
+        {
+            newProduct = p;
+        }
+
+        // команда добавления нового объекта
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      
+                  }));
+            }
+        }
+
+        private RelayCommand saveCommand;
+        public RelayCommand SaveCommand
+        {
+            get
+            {
+                return saveCommand ??
+                  (saveCommand = new RelayCommand(obj =>
+                  {
+                      
+                  }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Magazin/SweetRollWindow/ViewsModels/PokypkiVM.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+using BLL.Models;
+
+namespace SweetRollWindow.ViewsModels
+{
+    public class PokypkiVM : INotifyPropertyChanged
+    {
+
+        private PokypkaModel selectedPokypki;
+
+        public ObservableCollection<PokypkaModel> Pokypki { get; set; }
+        public PokypkaModel SelectedPokypki
+        {
+            get { return selectedPokypki; }
+            set
+            {
+                selectedPokypki = value;
+                OnPropertyChanged("SelectedPokypki");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+        // команда добавления нового объекта
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      PokypkaModel pokypki = new PokypkaModel();
+                      Pokypki.Insert(0, pokypki);
+                      SelectedPokypki = pokypki;
+                  }));
+            }
+        }
+        // команда удаления
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                  (removeCommand = new RelayCommand(obj =>
+                  {
+                      PokypkaModel pokypka = obj as PokypkaModel;
+                      if (pokypka != null)
+                      {
+                          Pokypki.Remove(pokypka);
+                      }
+                  },
+                 //условие, при котором будет доступна команда
+                 (obj) => (Pokypki.Count > 0 && selectedPokypki != null)));
+            }
+        }
+
+
+        public PokypkiVM()
+        {
+            DBDataOperation ClassWorkDB = new DBDataOperation();
+            Pokypki = new ObservableCollection<PokypkaModel>(ClassWorkDB.GetAllPokypki());
+
+
+        }
+
+    }
+}
